299ebbfbdff7b50e4c6b69abd0df41136cbfbd43
aminul124387@gmail.com <aminul124387@gmail.com>
2024-01-29 10:30:13 +0600

    Custom Inventory Dashboard

---
+++ static/src/components/kpi_card/kpi_card.js
@@ -0,0 +1,13 @@
+/** @odoo-module */
+
+const { Component } = owl
+
+export class KpiCard extends Component {}
+//export class KpiCard2 extends Component {}
+
+KpiCard.template = "owl.KpiCard"
+//KpiCard.template = "owl.KpiCard2"
+
+//export class KpiCardSingle extends Component {}
+//
+//KpiCard.template = "owl.KpiCardSingle"
